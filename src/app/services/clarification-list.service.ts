e6e510ec65ce7d91ae361d918c7fd4bc2a0b6116
LuisCldrn <Luis.cldrn@yahoo.com>
2023-05-11 14:47:55 -0500

    updated clarifications

--- src/app/services/clarification-list.service.ts
+++ src/app/services/clarification-list.service.ts
@@ -26,11 +26,9 @@ export class ClarificationListService {
   Epi = new Clarification('EPI', 'Missing/Invalid EpiPen Information', 'inActive', 'RX', 'required' );
   Referral = new Clarification('NEW REFERRAL', 'FOR NPE - NEED TO CREATE A CVS PRE-POPULATED FORM FOR VALID PRESCRIPTION AUTHORIZATION TO BE SENT TO MDO FOR APPROVAL', 'inActive', 'GEN', 'Nonrequired'); 
   NY = new Clarification('NY', 'NEW YORK PRESCRIBER/NEED A VERBAL PER NY MDO PROTOCOL RX', 'inActive', 'GEN', 'Nonrequired'); 
-  DS = new Clarification('DS', 'Missing/Invalid RX Day Supply', 'inActive', 'RX', 'required'); 
-  QTY = new Clarification('QTY', 'Missing/Invalid RX Quantity', 'inActive', 'RX', 'required'); 
+  DS = new Clarification('DISPENSE QTY/DS', 'Missing/Invalid RX Dispense Quantity/Day Supply', 'inActive', 'RX', 'required'); 
   vialSize = new Clarification('VIAL SIZE', 'Vial sizes and dose do not match', 'inActive', 'RX', 'Nonrequired'); 
   Dose = new Clarification('DOSE', 'Missing/Invalid Dose', 'inActive', 'RX', 'required');
-  DQ = new Clarification('DQ', 'Missing/Invalid Dispense Quantity', 'inActive', 'RX', 'required'); 
   Refills = new Clarification('REFILLS', 'Missing/Invalid Refills', 'inActive', 'RX', 'required'); 
   Drug = new Clarification('DRUG', 'Missing/Invalid main drug', 'inActive', 'RX', 'required'); 
   ROA = new Clarification('ROA', 'Missing/Invalid Route of Administration', 'inActive', 'RX', 'required'); 
@@ -43,9 +41,14 @@ export class ClarificationListService {
   ALTMed = new Clarification('ALT MED', 'Alternative Medication Needed', 'inActive', 'RX', 'Nonrequired'); 
   Freq = new Clarification('FREQ', 'Missing/Invalid Frequency', 'inActive', 'RX', 'Nonrequired'); 
   ChangeOrder = new Clarification('CHANGE IN ORDERS-RPH', 'Need RPh Review for Change in orders for __________', 'inActive', 'GEN', 'Nonrequired'); 
-
-
-  
+  preMedSig = new Clarification('PREMED-SIG', 'Need to clarify Directions on Pre-Medication _______', 'inActive', 'RX', 'Nonrequired');
+  preMedDose = new Clarification('PREMED-DOSE', 'Need to clarify Dose on Pre-Medication _______', 'inActive', 'RX', 'Nonrequired');
+  preMedRoa = new Clarification('PREMED-ROA', 'Need to clarify Rout of Admin on Pre-Medication _______', 'inActive', 'RX', 'Nonrequired');
+  SuppliesClause = new Clarification('SUPPLIES-Cluase', 'Need clause for authorization of ancillary supplies as needed for drug', 'inActive', 'GEN', 'Nonrequired'); 
+  FlushProt = new Clarification('FLUSH PROTOCOL', 'Missing/Invalid Flush Protocol on Rx', 'inActive', 'RX', 'Nonrequired'); 
+  Daw = new Clarification('DAW', 'Missing/Invalid DAW', 'inActive', 'RX', 'Nonrequired'); 
+  Lumens = new Clarification('LUMENS', 'Missing/Invalid Lumens', 'inActive', 'RX', 'Nonrequired'); 
+  Strength = new Clarification('STRENGTH', 'Missing/Invalid Strength on Main Drug', 'inActive', 'RX', 'Nonrequired'); 
   
   // new Clarification('', '', 'inActive', 'RX', 'Nonrequired'); 
 
@@ -60,15 +63,12 @@ export class ClarificationListService {
     this.PTIden,
     this.IgErx,
     this.Loa,
-    this.PreMed,
     this.Epi,
     this.Referral,
     this.NY,
     this.DS,
-    this.QTY,
     this.vialSize,
     this.Dose,
-    this.DQ,
     this.Refills,
     this.Drug,
     this.ROA,
@@ -81,5 +81,13 @@ export class ClarificationListService {
     this.ALTMed,
     this.Freq,
     this.ChangeOrder,
+    this.preMedDose,
+    this.preMedRoa,
+    this.preMedSig,
+    this.SuppliesClause,
+    this.FlushProt,
+    this.Daw,
+    this.Lumens,
+    this.Strength,
   ];
 }
